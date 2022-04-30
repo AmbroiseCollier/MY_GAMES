8f39b06d34ae79628d4d2fd1e13625084202c6b8
wilson cely <wectcorp@gmail.com>
2022-04-30 17:13:01 +0200

    update routes - review prof

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :players
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :games do
-    resources :reservations
+    resources :reservations, only: [:new,:create, :destroy]
+    resources :review, only: [:new,:create]
   end
 end
