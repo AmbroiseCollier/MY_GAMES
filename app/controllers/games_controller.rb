38beafa4962dedf17a64627f7054455eb9e1d8a5
Charles GrangerTheveniau <charlestheveniau@gmail.com>
2022-05-03 20:24:31 +0200

    added new and create method and views for Games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,17 @@
 class GamesController < ApplicationController
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.save
+    redirect_to root_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:address, :date)
+  end
 end
