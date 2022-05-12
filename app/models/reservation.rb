200ffb24d45f7157f033550d088da50281786750
wilson cely <wectcorp@gmail.com>
2022-05-12 18:50:03 +0200

    last changes

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :game
   belongs_to :participant, class_name: "Player", foreign_key: "player_id"
-  enum status: { requested: 0, accepted: 1, rejected: 2 }
+  enum status: { pending: 0, accepted: 1, rejected: 2 }
 
   validates :message, presence: true
 
