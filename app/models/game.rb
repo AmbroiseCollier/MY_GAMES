759fe680c88629135f79556ec95d20ae6ef5988d
wilson cely <wectcorp@gmail.com>
2022-05-12 12:31:29 +0200

    bug player-devises not able to save new fields in db- corrected

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,9 +2,9 @@ class Game < ApplicationRecord
   has_many :reservations
   has_many :players, through: :reservation, source: :participant
   has_many :reviews
-  validates :address, :date, :city, presence: true
   belongs_to :creator, class_name: "Player", foreign_key: "player_id"
 
+  validates :address, :date, :city, presence: true
   enum status: { free: 0, booked: 1 }
 
 end
