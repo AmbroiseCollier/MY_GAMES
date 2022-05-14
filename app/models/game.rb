59a1a331ae20e50438500240d97cd1584dda538d
wilson cely <wectcorp@gmail.com>
2022-05-14 16:51:40 +0200

    last corrections

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :players, through: :reservation, source: :participant
   has_many :reviews, dependent: :destroy
   belongs_to :creator, class_name: "Player", foreign_key: "player_id"
